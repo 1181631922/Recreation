43e6b630cdad8cd8b325e08b42906476b4892799
fanyafeng <fanyafeng@365rili.com>
2017-01-09 11:11:04 +0800

    添加首页readme

--- app/src/main/java/com/fanyafeng/recreation/bean/MainItemBean.java
+++ app/src/main/java/com/fanyafeng/recreation/bean/MainItemBean.java
@@ -7,44 +7,7 @@ import com.fanyafeng.recreation.util.StringUtil;
 
 import org.json.JSONObject;
 
-/**
- * Author： fanyafeng
- * Data： 16/11/9 14:57
- * Email: fanyafeng@live.cn
- * <p/>
- * "format": "image",
- * "image": "app117928293.jpg",
- * "published_at": 1478537402,
- * "tag": "",
- * "user": {
- * "avatar_updated_at": 1472121371,
- * "uid": 32508110,
- * "last_visited_at": 1472121371,
- * "created_at": 1472121371,
- * "state": "active",
- * "last_device": "ios_10.2.11",
- * "role": "",
- * "login": "\u718a\u718a\u4e8c\u718a\u718a\u4e8c",
- * "id": 32508110,
- * "icon": "2016082518361153.JPEG"
- * },
- * "image_size": {
- * "s": [197, 352, 12571],
- * "m": [420, 750, 49153]
- * },
- * "id": 117928293,
- * "votes": {
- * "down": -115,
- * "up": 1382
- * },
- * "created_at": 1478530801,
- * "content": "\u8fd8\u6709\u671f\u5f85\u7684\u5417",
- * "state": "publish",
- * "comments_count": 49,
- * "allow_comment": true,
- * "share_count": 374,
- * "type": "hot"
- */
+
 public class MainItemBean implements Parcelable {
 
     private String title;   //title有可能为空
